02f67d87899f5c1ffc7c712335448773a4381cc2
olahsara <129057913+olahsara@users.noreply.github.com>
2024-05-27 13:55:53 +0200

    firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,7 +6,8 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getVertexAI, provideVertexAI } from '@angular/fire/vertexai-preview';
+import {firebaseConfig} from "../ignore/firebase_options";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"jegyzi-2024","appId":"1:89465141980:web:35aae91f2964ac8f9f9219","storageBucket":"jegyzi-2024.appspot.com","apiKey":"AIzaSyDvZQX1t50xHZUE8feKl9sJJFsVFrWlIgw","authDomain":"jegyzi-2024.firebaseapp.com","messagingSenderId":"89465141980"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideVertexAI(() => getVertexAI())]
+  providers: [provideRouter(routes), provideFirebaseApp(() => initializeApp(firebaseConfig)), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideVertexAI(() => getVertexAI())]
 };
