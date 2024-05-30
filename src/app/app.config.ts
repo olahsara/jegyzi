e299c48a0cd5051c4f4756383281b81b1df6ffe4
Oláh Sára <saraolah20@gmail.com>
2024-05-30 16:17:31 +0200

    ..

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,7 +6,7 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getVertexAI, provideVertexAI } from '@angular/fire/vertexai-preview';
-import {firebaseConfig} from "../ignore/firebase_options";
+import { firebaseConfig } from "../ignore/firebase_options";
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import {MAT_TOOLTIP_DEFAULT_OPTIONS, MatTooltipDefaultOptions} from "@angular/material/tooltip";
 
--- src/app/login/pages/login/login.component.ts
+++ src/app/login/pages/login/login.component.ts
@@ -3,16 +3,17 @@ import { Component } from '@angular/core';
 import { TitleComponent } from '../../../shared/components/title/title.component';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'jegyzi-login',
   standalone: true,
   imports: [
-    CommonModule, TitleComponent, MatFormFieldModule, MatInputModule
+    CommonModule, TitleComponent, MatFormFieldModule, MatInputModule, ReactiveFormsModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent { 
-  
+export class LoginComponent {
+
 }
