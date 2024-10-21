94037b1e4e4fdb45055fba6472cc7f87cab168d4
Oláh Sára <saraolah20@gmail.com>
2024-10-21 16:53:37 +0200

    ..

--- src/app/note/pages/note-details-page/note-details-page.component.ts
+++ src/app/note/pages/note-details-page/note-details-page.component.ts
@@ -1,23 +1,42 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, effect, inject, signal, untracked } from '@angular/core';
+import { Timestamp } from '@angular/fire/firestore';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatInput } from '@angular/material/input';
 import { MatTooltip } from '@angular/material/tooltip';
 import { RouterLink } from '@angular/router';
 import { QuillEditorComponent } from 'ngx-quill';
 import { AvatarComponent } from '../../../shared/components/avatar/avatar/avatar.component';
+import { RatingComponent } from '../../../shared/components/rating-component/rating.component';
 import { TitleComponent } from '../../../shared/components/title/title.component';
 import { UserService } from '../../../shared/services/user.service';
+import { getName } from '../../../shared/utils/name';
 import { toDatePipe } from '../../pipes/to-date.pipe';
 import { NotePageService } from '../../services/note-page.service';
 
 @Component({
   selector: 'jegyzi-note-details-page',
   standalone: true,
-  imports: [CommonModule, TitleComponent, ReactiveFormsModule, RouterLink, AvatarComponent, toDatePipe, QuillEditorComponent, MatTooltip],
+  imports: [
+    CommonModule,
+    TitleComponent,
+    ReactiveFormsModule,
+    RouterLink,
+    AvatarComponent,
+    toDatePipe,
+    QuillEditorComponent,
+    MatTooltip,
+    MatExpansionModule,
+    MatCheckboxModule,
+    RatingComponent,
+    MatInput,
+  ],
   templateUrl: './note-details-page.component.html',
   styleUrl: './note-details-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  providers: [NotePageService, MatTooltip],
+  providers: [NotePageService],
 })
 export class NoteDetailsPageComponent {
   private userService = inject(UserService);
@@ -36,6 +55,15 @@ export class NoteDetailsPageComponent {
     profileType: new FormControl<string | null>(null),
     educationYear: new FormControl<number | null>(null),
     educationType: new FormControl<string | null>(null),
+  });
+
+  reviewForm = new FormGroup({
+    anonim: new FormControl<boolean | null>(null),
+    userId: new FormControl<string | null>(null),
+    userName: new FormControl<string | null>(null),
+    submitDate: new FormControl<Timestamp>(Timestamp.fromDate(new Date()) as Timestamp),
+    stars: new FormControl<number | null>(null),
+    description: new FormControl<string | null>(null),
   });
 
   constructor() {
@@ -62,5 +90,16 @@ export class NoteDetailsPageComponent {
     });
   }
 
+  selectStar(rating: number) {
+    this.reviewForm.controls.stars.setValue(rating);
+  }
+
   modifyRequest() {}
+
+  newReview() {
+    if (!this.reviewForm.value.anonim) {
+      this.reviewForm.controls.userId.setValue(this.loggedInUser()?.id!);
+      this.reviewForm.controls.userName.setValue(getName(this.loggedInUser()!));
+    }
+  }
 }
