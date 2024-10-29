6341c9aed867010738fda0a58b9283e9e79b7589
olahsara <129057913+olahsara@users.noreply.github.com>
2024-10-30 00:52:18 +0100

    hozzászólás

--- src/app/shared/models/comment.model.ts
+++ src/app/shared/models/comment.model.ts
@@ -4,9 +4,9 @@ export interface Comment {
   id: string;
   comment: string;
   date: Timestamp;
-  previousComment?: string; //comment id
   creatorId: string; //user id
-  creatorame: string;
+  creatorProfilPic: boolean;
+  creatorName: string;
   note: string; //note id
   //reagálás?
 }
